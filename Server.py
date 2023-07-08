8dce7467a64eee57e8056de9c624e4e82fb9c473
williamzabot <williamzabot123@gmail.com>
2023-07-07 21:02:53 -0300

    improvements

--- Server.py
+++ Server.py
@@ -1,23 +1,50 @@
 from socket import *
 from CustomPing import *
+from User import User
 
-# no servidor é utilizada a connection para enviar uma mensagem para o cliente
-# enquanto no cliente é utilizado o socketServer
+users = []
+
 myHost = 'localhost'
-myPort = 50001
+myPort = 1024
 socketServer = socket(AF_INET, SOCK_STREAM)
 socketServer.bind((myHost, myPort))
 socketServer.listen(1)
 print("Servidor na escuta...")
 connection, address = socketServer.accept()
-print('Server connected: ', connection)
-while True:
-    url = connection.recv(1024).decode("utf-8")
-    if url == ":q":
-        connection.close()
-        break
-    ping = str(doPing(url))
-    connection.send(ping.encode("utf-8"))
+def receive():
+    return str(connection.recv(1024).decode("utf-8"))
 
+def send(message):
+    connection.send(message.encode("utf-8"))
+def menu():
+    while True:
+        send("""
+1 - Registrar usuário 
+2 - Logar""")
+        clientMsg = receive()
+        if clientMsg == ":q":
+            connection.close()
+            break
+        elif clientMsg == "1":
+            user = User()
+            send("Digite o username: ")
+            user.username = receive()
+            send("Digite a senha: ")
+            user.password = receive()
+            users.append(user)
+        elif clientMsg == "2":
+            send("Digite o username: ")
+            username = receive()
+            send("Digite a senha: ")
+            password = receive()
+            for userDoArray in users:
+                if userDoArray.username == username:
+                    if userDoArray.password == password:
+                        send("Logado com sucesso! Digite uma url ")
+                        url = receive()
+                        ping = str(doPing(url))
+                        send(ping)
+        else:
+            print("escolha inválida")
 
-
+menu()
---
+++ User.py
@@ -0,0 +1,4 @@
+class User:
+    def __init__(self):
+        self.username = None
+        self.password = None
