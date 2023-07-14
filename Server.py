38285bb13b50702402893df7191999fb69ba89d3
williamzabot <williamzabot123@gmail.com>
2023-07-14 20:18:59 -0300

    melhorias

--- Server.py
+++ Server.py
@@ -2,6 +2,7 @@ from socket import *
 from CustomPing import *
 from User import User
 
+file = "ls.txt"
 users = []
 
 myHost = 'localhost'
@@ -20,10 +21,12 @@ def menu():
     while True:
         send("""
 1 - Registrar usuário 
-2 - Logar""")
+2 - Logar
+3 - ls""")
         clientMsg = receive()
         if clientMsg == ":q":
             connection.close()
+            print("encerrando..")
             break
         elif clientMsg == "1":
             user = User()
@@ -32,18 +35,34 @@ def menu():
             send("Digite a senha: ")
             user.password = receive()
             users.append(user)
+            print(f"{user.username} adicionado!")
         elif clientMsg == "2":
             send("Digite o username: ")
             username = receive()
             send("Digite a senha: ")
             password = receive()
+            userExist = False
             for userDoArray in users:
                 if userDoArray.username == username:
                     if userDoArray.password == password:
+                        userExist = True
                         send("Logado com sucesso! Digite uma url ")
                         url = receive()
                         ping = str(doPing(url))
-                        send(ping)
+                        #send(ping)
+                        print(ping)
+            if not userExist:
+                print("Usuário inválido")
+        elif clientMsg == "3":
+            command = f"ls > {file}"
+            os.system(command)
+            arq = open(file)
+            directories = ""
+            lines = arq.readlines()
+            for line in lines:
+                directories += line
+            #send(directories)
+            print(directories)
         else:
             print("escolha inválida")
 
